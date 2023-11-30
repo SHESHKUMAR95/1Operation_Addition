835c5ab0bfce371cc001e5b7be95e1da436cac13
SHESHKUMAR95 <148970064+SHESHKUMAR95@users.noreply.github.com>
2023-11-30 12:55:40 +0530

    Initial commit

---
+++ 1Operation_Addition/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Operation_Addition
+{
+    internal class Program
+    {
+        public void Addition()
+        {
+            int a = 10, b = 20;
+            int c = a + b;
+            Console.WriteLine("Addition : " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program op = new Program(); 
+            op.Addition();
+            Console.ReadLine(); 
+        }
+    }
+}
